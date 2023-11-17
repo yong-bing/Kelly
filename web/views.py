311a51406ecb385213e431c6a360cdc76cc604b4
冯永兵 <csufyb@163.com>
2023-11-17 17:47:17 +0800

    first commit

---
+++ web/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def resume(request):
+    return render(request, "resume.html")
+
+
+def services(request):
+    return render(request, "services.html")
+
+
+def portfolio(request):
+    return render(request, "portfolio.html")
+
+def contact(request):
+    return render(request, "contact.html")
